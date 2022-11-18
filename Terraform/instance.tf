c5c681721f35f3adf78da11ad651ccccfeebc31d
Shereef Assem <s.assem97@gmail.com>
2022-11-18 03:55:07 +0200

    fixing error in startup script

--- Terraform/instance.tf
+++ Terraform/instance.tf
@@ -35,7 +35,7 @@ resource "google_compute_instance" "privatevm" {
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+    sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
     sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
 
     EOF
