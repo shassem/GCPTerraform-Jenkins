3773e51d1e754d8924dc3dd0071770b6a9349227
Shereef Assem <s.assem97@gmail.com>
2022-11-18 21:32:55 +0200

    removing useless (sudo)

--- Terraform/instance.tf
+++ Terraform/instance.tf
@@ -30,13 +30,13 @@ resource "google_compute_instance" "privatevm" {
     startup-script = <<-EOF
 
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-    sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-    sudo apt-get update -y && sudo apt-get install ca-certificates curl gnupg lsb-release -y && sudo mkdir -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-    echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-    sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+    install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+    apt-get update -y && apt-get install ca-certificates curl gnupg lsb-release -y && mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+    apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
 
     EOF
   }
