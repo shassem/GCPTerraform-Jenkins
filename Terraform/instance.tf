2426d03f5bae4559a92cdf5877cda36595487a73
Shereef Assem <s.assem97@gmail.com>
2022-11-18 05:41:40 +0200

    fixing startup script

--- Terraform/instance.tf
+++ Terraform/instance.tf
@@ -31,11 +31,11 @@ resource "google_compute_instance" "privatevm" {
 
     curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
     sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-    sudo apt-get update && sudo apt-get install ca-certificates curl gnupg lsb-release && sudo mkdir -p /etc/apt/keyrings
+    sudo apt-get update -y && sudo apt-get install ca-certificates curl gnupg lsb-release -y && sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
     sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
 
     EOF
